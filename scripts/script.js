8723f5a6aa767d8a906b4a6e67fdf5b15e04d814
Алексей <[zyliano7@mail.ru]>
2025-02-05 19:40:48 +0300

    js

--- scripts/script.js
+++ scripts/script.js
@@ -36,7 +36,7 @@ function closeModal() {
 
 // Закрытие при нажатии на крестик или кнопку "Отмена"
 closeModalButton.onclick = closeModal;
-cancelModalButton.onclick = closeModal;
+
 
 // Закрытие при нажатии на полупрозрачную подложку
 window.onclick = function(event) {
@@ -48,38 +48,4 @@ window.onclick = function(event) {
 
 
 
-document.addEventListener('DOMContentLoaded', () => {
-  const swiper = new Swiper('.swiper-slider', {
-      direction: 'horizontal', // Горизонтальное направление
-      loop: true, // Зацикливание слайдов
-      slidesPerView: 1, // Показывать только один слайд
-      spaceBetween: 0, // Без промежутков между слайдами
 
-      pagination: {
-          el: '.swiper-pagination',
-          clickable: true,
-      },
-
-      navigation: {
-          nextEl: '.next',
-          prevEl: '.prev',
-      },
-  });
-});
-
-// Обработчики событий для кнопок навигации
-const prevButton = document.querySelector('.prev');
-const nextButton = document.querySelector('.next');
-
-// Проверяем, доступны ли кнопки в DOM
-if (prevButton && nextButton) {
-  prevButton.addEventListener('click', () => {
-      swiper.slidePrev(); // Переход к предыдущему слайду
-  });
-
-  nextButton.addEventListener('click', () => {
-      swiper.slideNext(); // Переход к следующему слайду
-  });
-} else {
-  console.error("Кнопки навигации не найдены в DOM.");
-}
---
+++ scripts/scripts.js
@@ -0,0 +1,38 @@
+
+
+
+document.addEventListener('DOMContentLoaded', () => {
+  const swiper = new Swiper('.swiper-slider', {
+      direction: 'horizontal', // Горизонтальное направление
+      loop: true, // Зацикливание слайдов
+      slidesPerView: 1, // Показывать только один слайд
+      spaceBetween: 0, // Без промежутков между слайдами
+
+      pagination: {
+          el: '.swiper-pagination',
+          clickable: true,
+      },
+
+      navigation: {
+          nextEl: '.next',
+          prevEl: '.prev',
+      },
+  });
+});
+
+// Обработчики событий для кнопок навигации
+const prevButton = document.querySelector('.prev');
+const nextButton = document.querySelector('.next');
+
+// Проверяем, доступны ли кнопки в DOM
+if (prevButton && nextButton) {
+  prevButton.addEventListener('click', () => {
+      swiper.slidePrev(); // Переход к предыдущему слайду
+  });
+
+  nextButton.addEventListener('click', () => {
+      swiper.slideNext(); // Переход к следующему слайду
+  });
+} else {
+  console.error("Кнопки навигации не найдены в DOM.");
+}
