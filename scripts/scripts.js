7caa48cc9c3a594842f3aca41d8fa1b275dde394
Алексей <[zyliano7@mail.ru]>
2025-03-25 00:28:08 +0300

    gg

--- scripts/scripts.js
+++ scripts/scripts.js
@@ -1,35 +1,35 @@
-  document.addEventListener('DOMContentLoaded', () => {
-        const swiper = new Swiper('.swiper-container', {
-          direction: 'horizontal', // Горизонтальное направление
-          loop: true, // Зацикливание слайдов
-          slidesPerView: 1, // Показывать только один слайд
-          spaceBetween: 0, // Без промежутков между слайдами
+document.addEventListener('DOMContentLoaded', () => {
+  const swiper = new Swiper('.swiper-container', {
+    direction: 'horizontal', // Горизонтальное направление
+    loop: true, // Зацикливание слайдов
+    slidesPerView: 1, // Показывать только один слайд
+    spaceBetween: 0, // Без промежутков между слайдами
 
-          pagination: {
-            el: '.swiper-pagination',
-            clickable: true,
-          },
+    pagination: {
+      el: '.swiper-pagination',
+      clickable: true,
+    },
 
-          navigation: {
-            nextEl: '.next',
-            prevEl: '.prev',
-          },
-        });
+    navigation: {
+      nextEl: '.next',
+      prevEl: '.prev',
+    },
+  });
 
-        // Обработчики событий для кнопок навигации
-        const prevButton = document.querySelector('.prev');
-        const nextButton = document.querySelector('.next');
+  // Обработчики событий для кнопок навигации
+  const prevButton = document.querySelector('.prev');
+  const nextButton = document.querySelector('.next');
 
-        // Проверяем, доступны ли кнопки в DOM
-        if (prevButton && nextButton) {
-          prevButton.addEventListener('click', () => {
-            swiper.slidePrev(); // Переход к предыдущему слайду
-          });
+  // Проверяем, доступны ли кнопки в DOM
+  if (prevButton && nextButton) {
+    prevButton.addEventListener('click', () => {
+      swiper.slidePrev(); // Переход к предыдущему слайду
+    });
 
-          nextButton.addEventListener('click', () => {
-            swiper.slideNext(); // Переход к следующему слайду
-          });
-        } else {
-          console.error('Кнопки навигации не найдены в DOM.');
-        }
-      });
+    nextButton.addEventListener('click', () => {
+      swiper.slideNext(); // Переход к следующему слайду
+    });
+  } else {
+    console.error('Кнопки навигации не найдены в DOM.');
+  }
+});
