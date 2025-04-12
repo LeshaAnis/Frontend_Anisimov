16859e3ab0c14d1cd0a7b0890cf670e92f31613f
Алексей <[zyliano7@mail.ru]>
2025-04-12 19:39:02 +0300

    12

--- app.ts
+++ app.ts
@@ -5,50 +5,6 @@ interface Card {
     imageUrl: string;
 }
 
-// Объект с данными карточек
-const cards: Record<string, Card> = {
-    card1: {
-        title: "Мир Танков",
-        description: "Многопользовательская онлайн-игра, в которой игроки управляют танками и сражаются друг с другом...",
-        imageUrl: "images/mir_tankov_1.jpg"
-    },
-    card2: {
-        title: "Escape from Tarkov",
-        description: "Реалистичный шутер от первого лица с элементами RPG и выживания...",
-        imageUrl: "images/tarkov_1.jpg"
-    },
-    card3: {
-        title: "Call of Duty: Warzone",
-        description: "Бесплатная королевская битва, в которой игроки сражаются на огромной карте...",
-        imageUrl: "images/warzone_2.jpg"
-    },
-    card4: {
-        title: "Grand Theft Auto V",
-        description: "Экшн-игра с открытым миром, в которой игроки могут исследовать вымышленный город Лос-Сантос...",
-        imageUrl: "images/Gta_5_1.jpg"
-    },
-    card5: {
-        title: "PUBG",
-        description: "Одна из первых игр в жанре королевской битвы...",
-        imageUrl: "images/pubg_1.jpg"
-    },
-    card6: {
-        title: "Overwatch",
-        description: "Overwatch — многопользовательская компьютерная игра в жанре геройского шутера...",
-        imageUrl: "images/overwatch.jpg"
-    },
-    card7: {
-        title: "Dota 2",
-        description: "Многопользовательская онлайн-игра в жанре MOBA...",
-        imageUrl: "images/dota_2_1.jpg"
-    },
-    card8: {
-        title: "Counter-Strike 2",
-        description: "Обновленная версия популярной серии шутеров от первого лица...",
-        imageUrl: "images/cs_2_1.jpg"
-    }
-};
-
 // Функция для генерации HTML-шаблона карточки
 function generateCardTemplate(card: Card): string {
     return `
@@ -61,7 +17,7 @@ function generateCardTemplate(card: Card): string {
 }
 
 // Функция для вставки карточек в указанный контейнер
-function insertCards(containerId: string): void {
+function insertCards(containerId: string, cards: Record<string, Card>): void {
     const container = document.getElementById(containerId);
 
     if (!container) {
@@ -81,7 +37,21 @@ function insertCards(containerId: string): void {
     }
 }
 
+// Функция для получения данных с сервера
+async function fetchCardsFromServer() {
+    try {
+        // Запрос к локальному JSON-файлу
+        const response = await fetch('games.json');
+        const data = await response.json();
+
+        // Вставка карточек в контейнер
+        insertCards('id_services_section', data);
+    } catch (error) {
+        console.error('Ошибка при получении данных:', error);
+    }
+}
+
 // Вызов функции после загрузки DOM
 document.addEventListener('DOMContentLoaded', () => {
-    insertCards('id_services_section');
+    fetchCardsFromServer();
 });
